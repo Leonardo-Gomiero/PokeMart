6b0594029f58a50c0c2d681c7d7591b16b93699c
Leonardo Gomiero <leoleonardo2000@gmail.com>
2023-09-05 21:39:05 -0300

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductRowComponent } from './product-row/product-row.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductRowComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
